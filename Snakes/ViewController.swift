abc5956b649cbf3bb42248020d3b1b0a50734f83
NNeyzi <naderneyzi@gmail.com>
2016-10-22 16:22:09 -0700

    first commit

---
+++ Snakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snakes
+//
+//  Created by Nader Neyzi on 10/22/16.
+//  Copyright © 2016 Nader Neyzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
